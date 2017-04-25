f8b729e8dff9be7a22e9d9e4a8e76f68db498e52
James <shortjn@dukes.jmu.edu>
2017-04-25 18:02:03 -0400

    Add files via upload
    Code was written with assistance from https://javascript.info/array

---
+++ collector.js
@@ -0,0 +1,10 @@
+let colors = ["Red", "Orange", "Yellow"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+choppingCart.push("Blue");
+
+// what's in colors?
+alert( colors.length ); // 4 
+
+// Program was written with assistance from https://javascript.info/array
